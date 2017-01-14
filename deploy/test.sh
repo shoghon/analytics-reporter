3fb4045dafdcf07567a836dd9c5830efea5c3ce2
Eric Mill <eric.mill@gsa.gov>
2017-01-14 17:23:35 -0500

    remove test script stuff

--- deploy/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# testing dap-1.env
-source $HOME/dap-1.env
-$HOME/bin/analytics --only=realtime --slim --verbose
-
-# testing dap-2.env
-source $HOME/dap-1.env
-$HOME/bin/analytics --only=realtime --slim --verbose
